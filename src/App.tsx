ddddf8a93d61922dc99b20f494b8cfb536e4372f
Lim-Heock <rnrwp30000@naver.com>
2025-12-08 09:34:04 +0900

    7.1강 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
 import { hourSelector, minuteState } from "./atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [minutes, setMinutes] = useRecoilState(minuteState); // atom의 값을 더해서 atom을 수정할 함수까지 준다는 것을 기억
   const hours = useRecoilValue(hourSelector);
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
+    setMinutes(+event.currentTarget.value); // 2.해당 값이 type : string 이기에 문제가 생김 -> + 을 넣어줌
   };
   return (
     <div>
@@ -14,7 +13,7 @@ function App() {
         value={minutes}
         onChange={onMinutesChange}
         type="number"
-        placeholder="Minutes"
+        placeholder="MInutes"
       />
       <input value={hours} type="number" placeholder="Hours" />
     </div>
