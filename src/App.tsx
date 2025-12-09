790459fa9be2c644411330ab2681b7caea8f36fe
DESKTOP-SK2DGE6\pc <rnrwp30000@naver.com>
2025-12-09 16:57:41 +0900

    7.2 내용

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-<<<<<<< HEAD
-=======
 import React from "react";
 import { useRecoilState } from "recoil";
->>>>>>> 64a5b4d (5.1 Set Selectors)
 import { hourSelector, minuteState } from "./atoms";
-import { useRecoilState, useRecoilValue } from "recoil";
 
 function App() {
-<<<<<<< HEAD
-  const [minutes, setMinutes] = useRecoilState(minuteState); // atom의 값을 더해서 atom을 수정할 함수까지 준다는 것을 기억
-  const hours = useRecoilValue(hourSelector);
-=======
   const [minutes, setMinutes] = useRecoilState(minuteState);
   const [hours, setHours] = useRecoilState(hourSelector);
->>>>>>> 64a5b4d (5.1 Set Selectors)
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value); // 2.해당 값이 type : string 이기에 문제가 생김 -> + 을 넣어줌
+    setMinutes(+event.currentTarget.value);
   };
   const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
     setHours(+event.currentTarget.value);
   };
   return (
     <div>
-      <input
-        value={minutes}
-        onChange={onMinutesChange}
-        type="number"
-        placeholder="MInutes"
-      />
+      <input type="number" placeholder="Minutes" />
+      <input value={hours} type="number" placeholder="Hours" />
       <input
         onChange={onHoursChange}
         value={hours}
--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,14 @@ import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "white",
+  textColor: "#f5f6fa",
   accentColor: "#9c88ff",
-  cardBgColor: "transparent",
+  cardBgColor: "rgba(0, 0, 0, 0.5)",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "#2f3640",
+  accentColor: "#9c88ff",
+  cardBgColor: "white",
 };
