72da681834cbfc7808f6c7e9bf0429f5824c8da7
Lim-Heock <rnrwp30000@naver.com>
2025-12-26 10:02:53 +0900

    DragConstraint, DragElastic, DragSnapToOrigin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion, Variants } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -17,13 +18,14 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const BiggerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const boxVariants: Variants = {
@@ -33,15 +35,21 @@ const boxVariants: Variants = {
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag" // white blue 같은 문자열은 인식을 잘 못함
-        whileTap="click"
-      ></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={1} //끌어당긴 힘
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag" // white blue 같은 문자열은 인식을 잘 못함
+          whileTap="click"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
